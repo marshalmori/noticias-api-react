489e37513b65e98cf595ebd80ccbc4698c57bfed
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-02-18 21:42:02 -0300

    Detectar a categoria selecionada

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -3,8 +3,16 @@ import { useState, useEffect, createContext } from "react";
 const NoticiasContext = createContext();
 
 const NoticiasProvider = ({ children }) => {
+  const [categoria, setCategoria] = useState("general");
+
+  const handleChangeCategoria = (e) => {
+    setCategoria(e.target.value);
+  };
+
   return (
-    <NoticiasContext.Provider value={{}}>{children}</NoticiasContext.Provider>
+    <NoticiasContext.Provider value={{ categoria, handleChangeCategoria }}>
+      {children}
+    </NoticiasContext.Provider>
   );
 };
 
